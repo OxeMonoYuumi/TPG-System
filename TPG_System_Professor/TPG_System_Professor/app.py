100810c39575c59c56847b0fd3f80331d25a3d06
Pedro Xavier <105009132+OxeMonoYuumi@users.noreply.github.com>
2025-05-12 22:23:23 -0300

    Delete TPG_System_Professor/TPG_System_Professor directory

--- TPG_System_Professor/TPG_System_Professor/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-from Controllers import routes
-app = Flask(__name__, template_folder='Views')
-
-routes.init_app(app)
-
-if __name__ == '__main__':
-    app.run(host='localhost', port=4000, debug=True)
